744b39f74022f5ca85ddc937a361356acf403ddb
яна <musina.yana02@yandex.ru>
2023-07-22 10:35:00 +0500

    commit

---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;
+   return N +SumNumbers(M,N-1);
+}
+
+int M = ReadInt("введите первое число");
+int N = ReadInt("введите второе число: ");
+if (M > N)
+{
+    System.Console.WriteLine("ввели неправильный диапазон");
+}
+System.Console.WriteLine("Сумма: "+ SumNumbers(M,N));
---
+++ task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int FunAckermana(int m, int n)
+{
+    if (m==0) return n+1;
+    else if (m>0 && n==0) return FunAckermana(m-1,1);
+    else {return FunAckermana (m-1,FunAckermana(m,n-1));}
+}
+int m =ReadInt("введите число m: ");
+int n =ReadInt("введите число n: ");
+if (m <0 && n<0)
+{
+    System.Console.WriteLine("ввели неверный числа");
+}
+System.Console.WriteLine("вывод: "+ FunAckermana(m,n));
+
